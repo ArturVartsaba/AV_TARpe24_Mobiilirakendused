fbf1202d45bbcfddd06f2c957bd27fa1d90d0d12
arturvartsaba <arturvartsaba1@gmail.com>
2026-02-04 11:33:25 +0200

    Esimene project. Tausta värvi muutmine, pildi liigutamine

--- TARpe24_Mobiilirakendused/MainPage.xaml.cs
+++ TARpe24_Mobiilirakendused/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TARpe24_Mobiilirakendused
+﻿using Android.Security.Identity;
+
+namespace TARpe24_Mobiilirakendused
 {
     public partial class MainPage : ContentPage
     {
@@ -17,8 +19,43 @@
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
+            BotImage.Rotation += 20;
+            BotImage.Opacity -= 0.1;
+            var rnd = new Random();
+            var rndColor = Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            BackgroundColor = rndColor; //muudab tausta värvi
+
+            if (count >= 5)
+            {
+                CounterBtn.BackgroundColor = Colors.Red;
+            }
+
+            if (count >= 10)
+            {
+                BotImage.IsVisible = false;
+                CounterBtn.Text = "Pilt kadus ära! Vajuta Reset.";
+            }
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnResetClicked(object? sender, EventArgs e)
+        {
+            count = 0;
+            CounterBtn.Text = "Alustame uuesti!";
+            BotImage.Rotation = 0;
+            BotImage.IsVisible = true;
+            CounterBtn.BackgroundColor = Colors.Blue;
+            BotImage.Opacity = 1;
+            BackgroundColor = Colors.White;
+
+            if (BotImage.HorizontalOptions == LayoutOptions.Start)
+            {
+                BotImage.HorizontalOptions = LayoutOptions.End;
+            }
+            else
+            {
+                BotImage.HorizontalOptions = LayoutOptions.Start;
+            }
         }
     }
 }
